e75991938f9abd40fb17a9c8304732b5a7397149
Yunusade <yunus.adebayo@sky.uk>
2023-11-09 11:04:48 +0000

    restaurants commit

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const app = require("./src/app")
 const db = require("./db/connection");
 const port = 3000;
-const Restaurant = require('./models')
+const Restaurant = require('./models');
 
 //TODO: Create your GET Request Route Below:
-
-app.get("/restaurants", async (req, res) => {
-    const allRestaurants = await Restaurant.findAll()
-    res.json(allRestaurants)
-})
-
-
 app.listen(port, () => {
     db.sync();
     console.log(`Listening at http://localhost:${port}/restaurants`);
--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
+app.get("/restaurants", async (req, res) => {
+    const allRestaurants = await Restaurant.findAll()
+    res.json(allRestaurants)
+})
+
+app.get("/restaurants/:id", async(req, res) => {
+    const id = req.params.id;
+    const json = await Restaurant.findByPk(id)
+    res.json(json)
+})
+
+app.post("/restaurants", async(req, res) => {
+    const restaurant = await Restaurant.create(req.body)
+    res.json(restaurant)
+})
+
+app.put('/restaurants/:id', async(req, res) => {
+    const updatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}}) 
+    res.json(updatedRestaurant)
+})
+
+app.delete('/restaurants/:id', async(req, res) => {
+    const deletedRestaurant = await Restaurant.destroy({where: {id: req.params.id}})
+    res.json(deletedRestaurant)
+})
 
 
 module.exports = app;
